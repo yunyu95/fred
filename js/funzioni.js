3f8a83dec3326073a8a1a781075564c13d013c71
yunyu <yu174ensol@live.it>
2019-10-23 17:29:16 +0200

    1729

--- js/funzioni.js
+++ js/funzioni.js
@@ -1,5 +1,8 @@
 function isEven(x) {
-    if (x === 0) {
+    if (x < 0) {
+        return undefined;
+    }
+    else if (x === 0) {
         return true;
     }
     else if (x % 2 === 1) {
@@ -8,11 +11,24 @@ function isEven(x) {
     else if (x % 2 === 0) {
         return true;
     }
-    else{
+    else {
         return undefined;
     }
 }
 
+function speed(distance, time) {
+    if (typeof (distance) != 'string' && typeof (time) != 'string' && typeof (distance) != 'boolean' && typeof (time) != 'boolean') {
+        if (distance < 0 || time < 0) {
+            return undefined;
+        }
+        else {
+            return distance / time;
+        }
+    }
+    else {
+        return undefined;
+    }
+}
 
 
 console.log(isEven(10) === true);
@@ -21,7 +37,20 @@ console.log(isEven(0) === true);
 console.log(isEven('r') === undefined);
 console.log(isEven() === undefined);
 console.log(isEven(3.4) === undefined);
+console.log(isEven(-42) === undefined);
 
 
+console.log(speed(-2, 10) === undefined);
+console.log(speed(2, -10) === undefined);
+console.log(speed(-2, -10) === undefined);
+console.log(speed('ciao', -10) === undefined);
+console.log(speed(-2, 'ciao') === undefined);
+console.log(speed('fo', 'vi') === undefined);
+console.log(speed('ciao', false) === undefined);
+console.log(speed(10, false) === undefined);
+console.log(speed(true, false) === undefined);
+console.log(speed(10, 10) === 1);
+console.log(speed(100, 10) === 10);
+console.log(speed(10, 100) === 0.1);
 
 
