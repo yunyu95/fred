1f42e1505c0e36646d9608e00860c26df86d43f0
yunyu <yu174ensol@live.it>
2019-10-24 17:39:32 +0200

    1755

--- js/newFun.js
+++ js/newFun.js
@@ -1,17 +1,49 @@
 function digitSum(number) {
-    if (number < 0) {
-        number = - number;
+    number = Math.abs(number);
+    let sum = 0;
+    while (number != 0) {
+        number = Math.floor(number);
+        sum += number % 10;
+        number /= 10;
     }
-    else {
-        let sum = 0;
-        while (number != 0) {
-            sum += number % 10;
-            number /= 10;
-        }
-        return sum;
-    }
+    return sum;
 }
 
-console.log(digitSum(111));
+/*function digitSum(number) {
+    number = Math.abs(number);
+    let numString = String(number);
+    let sum = 0;
+    for (let i = 0; i < numString.length; i++) {
+        num = Number(numString[i]);
+        sum += Number(numString[i]);
+    }
+    return sum;
+}*/
+
+function Singolo (num) {
+    let result = 0;
+    do {
+        result = digitSum(num);
+        num = result;
+    } while (result > 9);
+    return result;
+}
+
+/*console.log(digitSum(111));
 console.log(digitSum(7));
 console.log(digitSum(-56));
+console.log(digitSum(-99));
+console.log(digitSum(0));
+console.log(digitSum(324824));*/
+
+console.log(Singolo(111));
+console.log(Singolo(7));
+console.log(Singolo(-56));
+console.log(Singolo(-99));
+console.log(Singolo(0));
+console.log(Singolo(324824));
+
+
+
+
+
