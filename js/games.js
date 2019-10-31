45b741ef517ad54736e27ef6e2e5a8abd371443d
yunyu <yu174ensol@live.it>
2019-10-31 17:38:33 +0100

    1738

---
+++ js/games.js
@@ -0,0 +1,12 @@
+function betweenMarkers(text, begin, end) {
+    // returns substring between two given markers
+    let array = text.split(' ');
+    for (let i = 0; i < array.length; i++) {
+        
+    }
+    // your code here
+    return 0;
+}
+
+
+console.log(betweenMarkers('What is >apple<', '>', '<'));
